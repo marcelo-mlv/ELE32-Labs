9a3fde3dd187f138791f3afecce79c3cfd193b68
Luan-Horta <luanhorta16@gmail.com>
2025-03-28 23:06:55 -0300

    feat: add MyEncoder and MyDecoder

--- lab1/modules/Encoder.py
+++ lab1/modules/Encoder.py
@@ -48,3 +48,43 @@ class HammingEncoder(Encoder):
         """
         v = np.dot(u, self.G) % 2
         return v.astype(int)
+
+class MyEncoder(Encoder):
+    """
+    Receives an information, encodes it, transmits it through a channel and decodes it in an attempt to retrieve the original information.
+
+    It also uses a generator matrix G and a parity-check matrix H to encode and decode the information.
+
+    Typical usage example:
+
+    encoder = Encoder()
+    encoded_data = encoder.encode(raw_data)
+    transmitted_data = channel.transmit(encoded_data)
+    encoded_data_hat = encoder.decode(transmitted_data)
+    raw_data = encoded_data_hat[:4]
+    """
+
+    def __init__(self):
+        """
+        Initializes the Encoder with generator matrix G based on a generalization of Hamming Code for (9, 5)
+        """
+        self.G = np.array([
+            [1, 0, 0, 0, 0, 1, 0, 0, 1],
+            [0, 1, 0, 0, 0, 0, 1, 1, 1],
+            [0, 0, 1, 0, 0, 0, 1, 1, 0],
+            [0, 0, 0, 1, 0, 0, 1, 0, 1],
+            [0, 0, 0, 0, 1, 0, 0, 1, 1]
+        ])
+
+    def encode(self, u, *args, **kwargs):
+        """
+        Encodes an input message using the generator matrix.
+
+        Params:
+            u (list): List of bits to be encoded.
+
+        Returns:
+            v (list): Encoded list of bits.
+        """
+        v = np.dot(u, self.G) % 2
+        return v.astype(int)
