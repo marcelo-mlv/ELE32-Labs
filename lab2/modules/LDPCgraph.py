02953da8afedaf2d66f99cfdef8f442ed224b903
Luan-Horta <luanhorta16@gmail.com>
2025-04-30 02:44:44 -0300

    fix: table and arrays in LDPC graph

--- lab2/modules/LDPCgraph.py
+++ lab2/modules/LDPCgraph.py
@@ -8,8 +8,8 @@ class LDPCgraph:
         self.N = N
         self.M = M
 
-        self.table = np.array((M,N), dtype=int) # row = c-node, column = v-node
-        vnodes_num_conections = [0 for _ in range(N)]
+        self.table = np.zeros((M,N), dtype=int) # row = c-node, column = v-node
+        vnodes_num_conections = np.zeros(N)
         for m in range(M):
             chooseable_vnodes = [node for node in range(N) if vnodes_num_conections[node] == min(vnodes_num_conections)]
             chosen_vnodes = sorted(random.sample(chooseable_vnodes, dc))
@@ -17,14 +17,14 @@ class LDPCgraph:
                 self.table[m][vnode] = 1
                 vnodes_num_conections[vnode] += 1
 
-        self.vnodes_fails = np.array(N, dtype=int)
+        self.vnodes_fails = np.zeros(N, dtype=int)
 
     def check_node(self, m, input_bits):
-        result = np.dot(np.array(self.table[m]).T, input_bits) % 2
+        result = np.dot(self.table[m].T, input_bits) % 2
         if result == 1:
             for n in range(self.N):
                 if self.table[m][n] == 1:
                     self.vnodes_fails[n] += 1
 
     def reset_fails(self):
-        self.vnodes_fails = np.array(self.N, dtype=int)
+        self.vnodes_fails = np.zeros(self.N, dtype=int)
