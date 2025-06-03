960e148b3dcaf96c09756eac78af3b99f40f242d
Luan-Horta <luanhorta16@gmail.com>
2025-06-03 16:58:23 -0300

    mega giga ultra refatoração

--- lab3/main.py
+++ lab3/main.py
@@ -4,16 +4,15 @@ from modules.LDPC_LLR import LDPC_LLR
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 ### PARAMETERS ###
 # LDPC #
 dv = 3      # rate = 4/7 #
 dc = 7
-N = 994     #98, 994, 1001
-decode_max_iter = 50
+N = 1001   #98, 994, 1001
+decode_max_iter = 20
 # plot #
-samples = 5
-snr_values_dB = np.arange(0, 5.5, 0.5)
+samples = 100
+snr_values = np.arange(0, 5.5, 0.5)
 
 
 ### CHANNEL ###
@@ -23,18 +22,18 @@ channel = AWGNChannel()
 graph = LDPC_LLR(dv, dc, N)
 # OUTPUT - .CSV #
 graph.export_to_csv('ldpc_graph.csv')
-print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-print("ldpc_graph.csv [OK]\n")
+print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+print(f"ldpc_graph.csv [OK]\n")
 
-### PLOT ### 
-snr_values = 10 ** (snr_values_dB/10)               
-pb_values = np.zeros(len(snr_values))
+### PLOT ###              
+pb_ldpc_llr = np.zeros(len(snr_values))
 # INPUT #
 s_symbols = np.full(N, 1, dtype=int)
 
 for k in range(len(snr_values)):
 
-    Nzero = 1 / snr_values[k]
+    snr = 10 ** (snr_values[k]/10)
+    Nzero = 1 / snr
 
     ### SAMPLES ###
     num_of_flipped_symbols = 0
@@ -48,30 +47,31 @@ for k in range(len(snr_values)):
             if s == -1:
                 num_of_flipped_symbols += 1
 
-    pb_values[k] = num_of_flipped_symbols / (N*samples)
+    pb_ldpc_llr[k] = num_of_flipped_symbols / (N*samples)
 
-    print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-    print(f"Point no. {k+1} / {len(snr_values)}\n")
+    print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+    print(f"point no. {k+1} / {len(snr_values)}\n")
 
 ### OUTPUT - POINTS.TXT ###
-with open("output/points.txt", "w") as file:
-    for k in range(len(snr_values_dB)):
-        file.write("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-        file.write(f"Point no. {k+1}\n")
-        file.write(f"snr: {snr_values_dB[k]} dB\n")
-        file.write(f"pb: {pb_values[k]}\n")
-print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-print("points.txt [OK]\n")
+with open("output/ldpc_llr.txt", "w") as file:
+    for k in range(len(snr_values)):
+        file.write(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+        file.write(f"point no. {k+1}\n")
+        file.write(f"snr: {snr_values[k]}\n")
+        file.write(f"pb LDPC-LLR: {pb_ldpc_llr[k]}\n")
+print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+print(f"points.txt [OK]\n")
 
-### OUTPUT - PLOT.PNG ###
+### OUTPUT - POINTS.TXT ###
 plt.figure()
-plt.plot(snr_values_dB, pb_values)
+plt.plot(snr_values, pb_ldpc_llr)
 plt.yscale('log')
 plt.xlabel('snr (dB)')
 plt.ylabel('Pb')
-plt.title(f"AWGN channel / LDPC code, using LLR, rate = 4/7, N = {N}")
+plt.title("Prob. inversão de bit pós decodificação x relação sinal-ruído")
 plt.grid(True)
+# plt.legend()
 plt.savefig("output/plot.png", format="png")
-print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-print("plot.png [OK]\n")
+print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+print(f"plot.png [OK]\n")
 plt.show()
--- lab3/modules/LDPC_LLR.py
+++ lab3/modules/LDPC_LLR.py
@@ -3,6 +3,9 @@ import random
 import os
 import csv   
 
+CONNECTED = 0
+MESSAGE = 1
+
 class LDPC_LLR:
 
     def __init__(self, dv, dc, N):
@@ -11,18 +14,19 @@ class LDPC_LLR:
         self.N = N
         self.M = M
 
-        self.table = np.array([[(0, 0) for _ in range(N)] for _ in range(M)], dtype=object)
-        # adjacency matrix (M x N)  
-        # rows = c-nodes (M), columns = v-nodes (N), 
-        # tuple[0] = {1 if conected, else 0}, tuple[1] = LLR (message)
+        self.table = np.zeros((M, N, 2))
+        # table[m][n] = edge(m,n), m = c-node (M), n = v-node (N) 
+        # table[m][n][0] = 'is connected?' (0 [false] or 1 [true])
+        # table[m][n][1] = 'message' (LLR)
         
+        # randomly generated connections
         vnodes_num_conections = np.zeros(N)
         for m in range(M):
             chooseable_vnodes = [node for node in range(N) if vnodes_num_conections[node] == min(vnodes_num_conections)]
             chosen_vnodes = sorted(random.sample(chooseable_vnodes, dc))
-            for vnode in chosen_vnodes:
-                self.table[m][vnode][0] = 1
-                vnodes_num_conections[vnode] += 1
+            for n in chosen_vnodes:
+                self.table[m][n][CONNECTED] = 1
+                vnodes_num_conections[n] += 1
     
 
     def export_to_csv(self, filename):
@@ -30,98 +34,87 @@ class LDPC_LLR:
         with open(path, mode='w', newline='') as file:
             writer = csv.writer(file)
             for n in range(self.N):
-                connected_cnodes = [(m+1) for m in range(self.M) if self.table[m][n][0] == 1]   # i = 1,...,N
-                writer.writerow(connected_cnodes)
+                connected_cnodes = np.where(self.table[:, n, CONNECTED] == 1)[0]
+                writer.writerow(connected_cnodes + 1)    # i = 1,...,M
     
     # decode
     
     def decode(self, channel_input, Nzero, max_iter=20):
 
-        self.symbols = channel_input
-        self.channel_llrs = self.llr(Nzero)
-        # LLRs from the channel input
+        self.calculate_symbols_llrs(channel_input, Nzero)
 
         for _ in range(max_iter):
 
-            for n in range(self.N):
-                self.consensus_propagation(n)
+            self.consensus_propagation()
 
-            is_word_valid = True
-            for m in range(self.M):
-                if self.is_cnode_valid(m) == False:
-                    is_word_valid = False
-                    break
-            if is_word_valid == True: break
+            if self.is_word_valid(): break
 
-            for m in range(self.M):
-                self.discord_propagation(m)
+            self.discord_propagation()
 
-        for n in range(self.N):
-            self.symbol_decision(n)
+        output = self.symbols_decision()
 
         self.reset_messages()
 
-        return self.symbols
+        return output
     
-    def llr(self, Nzero):
-        llr = np.array([], dtype=float)
-        for r in self.symbols:
-            llr = np.append(llr, 4 * r / Nzero)       # L = 2*r/var = 4*r/N0
-        return llr
+    def calculate_symbols_llrs(self, symbols, Nzero):
+        self.llrs = symbols * 4 / Nzero
     
-    def consensus_propagation(self, n):
-        conections_idx = np.array([], dtype=int)
-        sum = self.channel_llrs[n]
-        for m in range(self.M):
-            if self.table[m][n][0] == 1:
-                conections_idx = np.append(conections_idx, m)
-                sum += self.table[m][n][1]
-        for m in conections_idx:
-            self.table[m][n][1] = sum - self.table[m][n][1]
+    def consensus_propagation(self):
+        for n in range(self.N):
+            # Índices dos c-nodes conectados ao v-node n
+            cnodes = np.where(self.table[:, n, CONNECTED] == 1)[0]  # pega todos m tal que table[m][n][0] == 1
+            # Mensagens recebidas de cada c-node para o v-node n
+            cnodes_messages = self.table[cnodes, n, MESSAGE]
+            # Soma total das mensagens + LLR do canal
+            total = np.sum(cnodes_messages, axis=0) + self.llrs[n]
+            # Para cada c-node conectado, atualiza mensagem extrínseca
+            self.table[cnodes, n, MESSAGE] = total - cnodes_messages
         return
     
-    def discord_propagation(self, m):
-        conections_idx = np.array([], dtype=int)
-        for n in range(self.N):
-            if self.table[m][n][0] == 1:
-                conections_idx = np.append(conections_idx, n)
-        for n in conections_idx:
-            discord = 1
-            current_min = np.inf
-            for i in conections_idx:
-                if i != n:
-                    llr = self.table[m][i][1]
-                    discord *= np.sign(llr)
-                    if llr < current_min:
-                        current_min = llr
-            discord *= current_min
-            self.table[m][n][1] = discord
+    def discord_propagation(self):
+        for m in range(self.M):
+            vnodes = np.where(self.table[m, :, CONNECTED] == 1)[0]
+            vnodes_messages = self.table[m, vnodes, MESSAGE]
+            new_vnodes_messages = np.copy(vnodes_messages)
+            for i in range(len(vnodes)):
+                others_messages = np.delete(vnodes_messages, i)
+                discord = np.prod(np.sign(others_messages)) * np.min(np.abs(others_messages))
+                new_vnodes_messages[i] = discord
+            self.table[m, vnodes, MESSAGE] = new_vnodes_messages
         return
     
     def is_cnode_valid(self, m):
-        product = 1
-        valid = True
-        for n in range(self.N):
-            if self.table[m][n][0] == 1:
-                product *= np.sign(self.table[m][n][1])
-        if product == -1:
+        vnodes = np.where(self.table[m, :, CONNECTED] == 1)[0]
+        vnodes_messages = self.table[m, vnodes, MESSAGE]
+        product = np.prod(np.sign(vnodes_messages))
+        if product == 1:
+            valid = True
+        else:
             valid = False
         return valid
     
-    def symbol_decision(self, n):
-        sum = self.channel_llrs[n]
+    def is_word_valid(self):
+        valid = True
         for m in range(self.M):
-            if self.table[m][n][0] == 1:
-                sum += self.table[m][n][1]
-        if sum >= 0:
-            self.symbols[n] = 1
-        else:
-            self.symbols[n] = -1
-        return
+            if self.is_cnode_valid(m) == False:
+                valid = False
+                break
+        return valid
+    
+    def symbols_decision(self):
+        output = np.zeros(self.N)
+        for n in range(self.N):
+            cnodes = np.where(self.table[:, n, CONNECTED] == 1)[0]  
+            cnodes_messages = self.table[cnodes, n, MESSAGE] 
+            total = np.sum(cnodes_messages, axis=0) + self.llrs[n]
+            if total >= 0:
+                output[n] = 1
+            else:
+                output[n] = -1
+        return output
 
     def reset_messages(self):
-        for m in range(self.M):
-            for n in range(self.N):
-                if self.table[m][n][0] == 1:    # if is a conection
-                    self.table[m][n][1] = 0     # reset message
+        mask = self.table[:, :, CONNECTED] == 1
+        self.table[:, :, MESSAGE][mask] = 0
         return
