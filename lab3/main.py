21b75079b604eaf44dd4ceb25be23139f583cc9f
Luan-Horta <luanhorta16@gmail.com>
2025-06-02 22:37:18 -0300

    refactor: main: samples added

--- lab3/main.py
+++ lab3/main.py
@@ -4,56 +4,61 @@ from modules.LDPC_LLR import LDPC_LLR
 import numpy as np
 import matplotlib.pyplot as plt
 
-
+### CHANNEL ###
 channel = AWGNChannel()
 
+### LDPC ###
+# rate = 4/7 #
 dv = 3
 dc = 7
 N = 994 #98, 994, 1001
-decode_max_iter = 20
+decode_max_iter = 50
 graph = LDPC_LLR(dv, dc, N)
+# EXPORT #
 graph.export_to_csv('ldpc_graph.csv')
 
-snr_values_dB = np.arange(-2, 5.5, 0.5) 
+### PLOT ###
+snr_values_dB = np.arange(0, 5.5, 0.5) 
 snr_values = 10 ** (snr_values_dB/10)               
 pb_values = np.zeros(len(snr_values))
-
+samples = 5
+# INPUT #
 s_symbols = np.full(N, 1, dtype=int)
 
 for k in range(len(snr_values)):
 
     Nzero = 1 / snr_values[k]
-    r_symbols = channel.transmit(s_symbols, Nzero)
 
-    decoded_symbols = graph.decode(r_symbols, Nzero, decode_max_iter)
-
+    ### SAMPLES ###
     num_of_flipped_symbols = 0
-    for s in decoded_symbols:
-        if s == -1:
-            num_of_flipped_symbols += 1
-    pb_values[k] = num_of_flipped_symbols / N
+    for _ in range(samples):
+    
+        r_symbols = channel.transmit(s_symbols, Nzero)
+
+        decoded_symbols = graph.decode(r_symbols, Nzero, decode_max_iter)
+
+        for s in decoded_symbols:
+            if s == -1:
+                num_of_flipped_symbols += 1
+
+    pb_values[k] = num_of_flipped_symbols / (N*samples)
 
     print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
-    print(f"Iteration no. {k+1}")
-    # print(f"r: {r_symbols}")
-    # print(f"decoded: {decoded_symbols}")
+    print(f"Point no. {k+1}")
     print(f"snr: {snr_values_dB[k]} dB")
     print(f"pb: {pb_values[k]}")
 
 """
 Plots the bit error probability as a function of the signal-to-noise ratio (dB).
-    x axis: snr_values, in logarithmic scale
     y axis: pb_values, in logarithmic scale
+    x axis: snr_values_dB
 """
 plt.figure()
 plt.plot(snr_values_dB, pb_values)
-# plt.xscale('log')
 plt.yscale('log')
 plt.xlabel('snr (dB)')
 plt.ylabel('Pb')
-plt.title("Prob. inversão de bit pós decodificação x relação sinal-ruído")
+plt.title("AWGN channel / LDPC code, using LLR, rate = 4/7, N = 994")
 plt.grid(True)
-# plt.legend()
-# plt.gca().invert_xaxis()
 plt.savefig("plot.png", format="png")
 plt.show()
