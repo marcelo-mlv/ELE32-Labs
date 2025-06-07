23fa4ee10ba85fda6f68433503dad8d2d509d2f1
Marcelo <marceloloiolaveras@gmail.com>
2025-06-07 17:23:51 -0300

    refactor: optimize simulation functions and streamline error calculation in run_simulation.py

--- lab4/main.py
+++ lab4/main.py
@@ -3,11 +3,35 @@ from modules.run_simulation import run_bpsk, run_ldpc_bp, run_ldpc_bf, run_hammi
 from params import rate, decode_max_iter, num_samples, snr_values, \
 awgnc, bsc, ldpc_bp, ldpc_bf, hamming, s_bits, s_symbols
 
+import math
+import numpy as np
+
+pb_bpsk = []
+pb_ldpc_bp = []
+pb_ldpc_bf = []
+pb_hamming = []
+Q = lambda x: 0.5 * (1 - math.erf(x / np.sqrt(2)))
+
 ### Simulation ###
-pb_bpsk = run_bpsk(snr_values)
-pb_ldpc_bp = run_ldpc_bp(s_symbols, awgnc, ldpc_bp, snr_values, decode_max_iter, rate, num_samples)
-pb_ldpc_bf = run_ldpc_bf(s_bits, bsc, ldpc_bf, snr_values, decode_max_iter, num_samples)
-pb_hamming = run_hamming(s_bits, bsc, hamming, snr_values, num_samples)
+for idx, snr_db in enumerate(snr_values):
+    snr = 10 ** (snr_db / 10)
+    p_bsc = Q(math.sqrt(3*snr))
+    N0 = 1/(rate*snr)
+
+    bp_flipped_symbols = 0
+    bf_flipped_symbols = 0
+    hm_flipped_symbols = 0
+
+    for _ in range(num_samples):
+        bp_flipped_symbols += np.sum(run_ldpc_bp(s_symbols, awgnc, ldpc_bp, N0, decode_max_iter) == -1)
+        bf_flipped_symbols += np.sum(run_ldpc_bf(s_bits, bsc, ldpc_bf, p_bsc, decode_max_iter) == 1)
+        hm_flipped_symbols += np.sum(run_hamming(s_bits, bsc, hamming, p_bsc) == 1)
+
+    pb_bpsk.append(run_bpsk(snr))
+    pb_ldpc_bp.append(bp_flipped_symbols / (num_samples * len(s_symbols)))
+    pb_ldpc_bf.append(bf_flipped_symbols / (num_samples * len(s_bits)))
+    pb_hamming.append(hm_flipped_symbols / (num_samples * len(s_bits)))
+
 
 ### Output ###
 out_txt(snr_values, pb_bpsk, pb_ldpc_bf, pb_hamming, pb_ldpc_bp)
--- lab4/modules/run_simulation.py
+++ lab4/modules/run_simulation.py
@@ -3,54 +3,24 @@ import math
 
 Q = lambda x: 0.5 * (1 - math.erf(x / np.sqrt(2)))
 
-def run_bpsk(snr_values):
-    pb_bpsk = np.zeros(len(snr_values))
-    for k, snr_db in enumerate(snr_values):
-        snr = 10 ** (snr_db/10)
-        pb_bpsk[k] = Q(np.sqrt(3*snr))
-    return pb_bpsk
+def run_bpsk(snr):
+    return Q(math.sqrt(3*snr))
 
-def run_ldpc_bp(s_symbols, channel, ldpc_bp, snr_values, decode_max_iter, rate, num_samples):
-    N = len(s_symbols)
-    pb_ldpc_bp = np.zeros(len(snr_values))
-    for k, snr_db in enumerate(snr_values):
-        snr = 10 ** (snr_db/10)
-        Eb = 1 / rate
-        Nzero = Eb / snr
-        num_errors = 0
-        for _ in range(num_samples):
-            r_symbols = channel.transmit(s_symbols, Nzero)
-            bp_decoded_symbols = ldpc_bp.decode(r_symbols, Nzero, decode_max_iter)
-            num_errors += np.sum(bp_decoded_symbols == -1)
-        pb_ldpc_bp[k] = num_errors / (N * num_samples)
-    return pb_ldpc_bp
+def run_ldpc_bp(s_symbols, channel, ldpc_bp, Nzero, decode_max_iter):
+    r_symbols = channel.transmit(s_symbols, Nzero)
+    bp_decoded_symbols = ldpc_bp.decode(r_symbols, Nzero, decode_max_iter)
+    return bp_decoded_symbols
 
-def run_ldpc_bf(s_bits, channel, ldpc_bf, snr_values, decode_max_iter, num_samples):
-    N = len(s_bits)
-    pb_ldpc_bf = np.zeros(len(snr_values))
-    for k, snr_db in enumerate(snr_values):
-        snr = 10 ** (snr_db/10)
-        p = Q(np.sqrt(3*snr))
-        num_errors = 0
-        for _ in range(num_samples):
-            r_bits = channel.transmit(s_bits, p)
-            bf_decoded_bits = ldpc_bf.decode(r_bits, decode_max_iter)
-            num_errors += np.sum(bf_decoded_bits == 1)
-        pb_ldpc_bf[k] = num_errors / (N * num_samples)
-    return pb_ldpc_bf
+def run_ldpc_bf(s_bits, channel, ldpc_bf, p, decode_max_iter):
+    r_bits = channel.transmit(s_bits, p)
+    bf_decoded_bits = ldpc_bf.decode(r_bits, decode_max_iter)
+    return bf_decoded_bits
 
-def run_hamming(s_bits, channel, hamming, snr_values, num_samples):
-    N = len(s_bits)
-    pb_hamming = np.zeros(len(snr_values))
-    for k, snr_db in enumerate(snr_values):
-        snr = 10 ** (snr_db/10)
-        p = Q(np.sqrt(3*snr))
-        num_errors = 0
-        for _ in range(num_samples):
-            r_bits = channel.transmit(s_bits, p)
-            for i in range(0, len(r_bits), 7):
-                chunk = r_bits[i:i+7]
-                chunk_decoded_bits = hamming.decode(chunk)
-                num_errors += np.sum(chunk_decoded_bits == 1)
-        pb_hamming[k] = num_errors / (N * num_samples)
-    return pb_hamming
+def run_hamming(s_bits, channel, hamming, p):
+    r_bits = channel.transmit(s_bits, p)
+    hamming_decoded_bits = np.array([], dtype=int)
+    for i in range(0, len(r_bits), 7):
+        chunk = r_bits[i:i+7]
+        chunk_decoded_bits = hamming.decode(chunk)
+        hamming_decoded_bits = np.concatenate((hamming_decoded_bits, chunk_decoded_bits))
+    return hamming_decoded_bits
