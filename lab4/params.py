5a0c8983509515d62a8b78b474a81732e000cba0
Luan-Horta <luanhorta16@gmail.com>
2025-07-01 23:25:57 -0300

    bora

--- lab4/params.py
+++ lab4/params.py
@@ -8,38 +8,36 @@ import numpy as np
 import math
 
 ### CONSTANTS ###
-Q = lambda x: 0.5 * (1 - math.erf(x / np.sqrt(2)))
+Q = lambda x: 0.5 * np.exp(- (x ** 2) / 2)
 
 ### PARAMETERS ###
 rate = 4/7
 
 # LDPC #
-dv = 3
-dc = 7
 N = 1001
+N_2 = 1000
 decode_max_iter = 20
 
 # simulation #
-num_samples = 1
-snr_values = np.arange(0, 5.5, 0.5)
-
-print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
-print(f"dv        = {dv}\n")
-print(f"dc        = {dc}\n")
-print(f"N         = {N}\n")
-print(f"samples   = {num_samples}\n")
+num_samples = 10
+Et_N0_values_sem_cod = np.arange(0, 10.5, 0.5)
+Et_N0_values = Et_N0_values_sem_cod - 3
 
 ### CHANNEL ###
 awgnc = AWGNChannel()
 bsc = BinarySymmetricChannel()
 
 ### LDPC-BP ###
-ldpc_bp = LDPC_BP.from_csv('ldpc_graph.csv', dv, dc)
+ldpc_bp = LDPC_BP.from_csv('graph_3_7.csv', dv=3, dc=7)
 print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
 print(f"LDPC-BP GRAPH BUILT\n")
 
+ldpc_bp_2 = LDPC_BP.from_csv('graph_4_8.csv', dv=4, dc=8)
+print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
+print(f"LDPC-BP-2 GRAPH BUILT\n")
+
 ## LDPC-BF ###
-ldpc_bf = LDPC_BF.from_csv('ldpc_graph.csv', dv, dc)
+ldpc_bf = LDPC_BF.from_csv('ldpc_graph.csv', dv=3, dc=7)
 print(f"=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n")
 print(f"LDPC-BF GRAPH BUILT\n")
 
